9f42b6581e1f330d5fa8d67a47bdc19d9500cead
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-19 22:56:43 +0500

    practiced arithmetic operators in java

---
+++ Foundation/ArithmeticOperators.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
