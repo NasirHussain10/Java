a5356eb3418967aaacc9551c2a9c4bc05e94c7d8
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-19 22:51:16 +0500

    Uploaded file containing data types and variables in java

---
+++ Foundation/DataTypesAndVariables.java
@@ -0,0 +1,10 @@
+public class DataTypesAndVariables {
+    public static void main(String[] args) {
+        int age = 18;
+        double GPA = 3.9;
+        char ch = 'N';
+        String name = "Nasir";
+
+        System.out.println("MY name is " +name+ " and I'm " +age+ " years old. My CGPA is " +GPA+ " and My name's first leeter is " +ch);
+    }
+}
