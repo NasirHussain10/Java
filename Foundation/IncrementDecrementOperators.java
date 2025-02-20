218bb9f683d59183759b18711f258b5bda47df4e
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-20 09:09:17 +0500

    Uploaded file containing Increament and Decrement operators

---
+++ Foundation/IncrementDecrementOperators.java
@@ -0,0 +1,17 @@
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+        int d = 5;
+        
+        d++;
+        System.out.println(d);
+        
+        d--;
+        System.out.println(d);
+
+        ++d;
+        System.out.println(d);
+        
+        --d;
+        System.out.println(d);
+    }
+}
