2bca914f5d394bfb21b4eb9570115609f01ee401
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-20 09:06:51 +0500

    Used assignment operators in java

---
+++ Foundation/AssignmentOperators.java
@@ -0,0 +1,9 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int c = 5;
+        
+        c += 3;
+        c -= 2;
+        System.out.println(c);
+    }
+}
