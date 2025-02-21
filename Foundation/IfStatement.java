2459dc91219a557971bef74936913b09de9edfb8
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-21 15:32:09 +0500

    Added IfStaement file

---
+++ Foundation/IfStatement.java
@@ -0,0 +1,9 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int num = 10;
+        
+        if (num <=10) {
+            System.out.println("Entered number is less than or equals to 10.");
+        }
+    }
+}
