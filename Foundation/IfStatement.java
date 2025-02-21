4bf4d6a9d42e62f6c848d43d3c6f9648869238f6
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-21 15:25:24 +0500

    Uploaded file containing if-else statement

---
+++ Foundation/IfStatement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age ;
+        
+        System.out.println("Enter your age: ");
+        age = scan.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        }else{
+            System.out.println("You are under age");
+        }
+    }
+}
