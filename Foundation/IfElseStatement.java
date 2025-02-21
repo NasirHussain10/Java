27d86c751b3833ad8bec3e62ff137a028810599d
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-02-21 15:28:26 +0500

    change the IfStatement file name to IfElseStatement

--- Foundation/IfElseStatement.java
+++ Foundation/IfElseStatement.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class IfStatement {
+public class IfElseStatement {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int age ;
