799842c1105a45fccd6f35f822e5acdd8ce7f65f
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-04-23 11:30:58 +0500

    This contain a program consist of a method wth no parameter and no return type

---
+++ Foundation/NoReturnNoParaMethod.java
@@ -0,0 +1,10 @@
+class hello {
+    public static void greet() {
+        System.out.println("Hello, welcome!");
+    }
+}
+public class NoReturnNoParaMethod{
+    public static void main(String[] args) {
+        hello.greet();
+    }
+}
